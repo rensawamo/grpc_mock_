7e8787067d5ded3ea0590799893f64e2a2b7c509
rensawamo <ren0112ngzk@gmail.com>
2024-03-29 15:04:21 +0900

    integration test の追加 #4

--- frontend/integration_test/app_test.dart
+++ frontend/integration_test/app_test.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/src/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '../lib/main.dart'; 
+import '../lib/main.dart';
 
 void main() {
   group('App Integration Tests', () {
     testWidgets('MyApp loads and displays correctly', (tester) async {
-      // ProviderScopeでアプリケーションをラップする
+
+      // セットアップ
       await tester.pumpWidget(
         const ProviderScope(
           child: MyApp(),
         ),
       );
+      await tester.pumpAndSettle();
+      await tester.pump(Duration(seconds: 2));
+
+      
       expect(find.text('todo1'), findsOneWidget);
+
     });
   });
 }
