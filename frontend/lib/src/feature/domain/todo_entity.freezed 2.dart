2c5f6a4731f6d6415c12019d99661eb1d2aa6858
rensawamo <ren0112ngzk@gmail.com>
2024-03-29 18:25:30 +0900

    build runnerのトラブル

---
+++ frontend/lib/src/feature/domain/todo_entity.freezed 2.dart
@@ -0,0 +1,200 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'todo_entity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+TodoModel _$TodoModelFromJson(Map<String, dynamic> json) {
+  return _TodoModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$TodoModel {
+  String get id => throw _privateConstructorUsedError;
+  String get content => throw _privateConstructorUsedError;
+  @CompletionStatusConverter()
+  CompletionStatus get status => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $TodoModelCopyWith<TodoModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TodoModelCopyWith<$Res> {
+  factory $TodoModelCopyWith(TodoModel value, $Res Function(TodoModel) then) =
+      _$TodoModelCopyWithImpl<$Res, TodoModel>;
+  @useResult
+  $Res call(
+      {String id,
+      String content,
+      @CompletionStatusConverter() CompletionStatus status});
+}
+
+/// @nodoc
+class _$TodoModelCopyWithImpl<$Res, $Val extends TodoModel>
+    implements $TodoModelCopyWith<$Res> {
+  _$TodoModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? content = null,
+    Object? status = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      content: null == content
+          ? _value.content
+          : content // ignore: cast_nullable_to_non_nullable
+              as String,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as CompletionStatus,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$TodoModelImplCopyWith<$Res>
+    implements $TodoModelCopyWith<$Res> {
+  factory _$$TodoModelImplCopyWith(
+          _$TodoModelImpl value, $Res Function(_$TodoModelImpl) then) =
+      __$$TodoModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String id,
+      String content,
+      @CompletionStatusConverter() CompletionStatus status});
+}
+
+/// @nodoc
+class __$$TodoModelImplCopyWithImpl<$Res>
+    extends _$TodoModelCopyWithImpl<$Res, _$TodoModelImpl>
+    implements _$$TodoModelImplCopyWith<$Res> {
+  __$$TodoModelImplCopyWithImpl(
+      _$TodoModelImpl _value, $Res Function(_$TodoModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? content = null,
+    Object? status = null,
+  }) {
+    return _then(_$TodoModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      content: null == content
+          ? _value.content
+          : content // ignore: cast_nullable_to_non_nullable
+              as String,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as CompletionStatus,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$TodoModelImpl extends _TodoModel {
+  const _$TodoModelImpl(
+      {required this.id,
+      required this.content,
+      @CompletionStatusConverter() required this.status})
+      : super._();
+
+  factory _$TodoModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$TodoModelImplFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String content;
+  @override
+  @CompletionStatusConverter()
+  final CompletionStatus status;
+
+  @override
+  String toString() {
+    return 'TodoModel(id: $id, content: $content, status: $status)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TodoModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.content, content) || other.content == content) &&
+            (identical(other.status, status) || other.status == status));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, content, status);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$TodoModelImplCopyWith<_$TodoModelImpl> get copyWith =>
+      __$$TodoModelImplCopyWithImpl<_$TodoModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$TodoModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _TodoModel extends TodoModel {
+  const factory _TodoModel(
+      {required final String id,
+      required final String content,
+      @CompletionStatusConverter()
+      required final CompletionStatus status}) = _$TodoModelImpl;
+  const _TodoModel._() : super._();
+
+  factory _TodoModel.fromJson(Map<String, dynamic> json) =
+      _$TodoModelImpl.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get content;
+  @override
+  @CompletionStatusConverter()
+  CompletionStatus get status;
+  @override
+  @JsonKey(ignore: true)
+  _$$TodoModelImplCopyWith<_$TodoModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
